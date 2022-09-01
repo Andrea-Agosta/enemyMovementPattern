2b86b959c42ab4b41dafce2fc47aa6d1158e1606
andrea-agosta-salt <andrea.agosta@appliedtechnology.se>
2022-09-01 15:58:25 +0200

    complete first animation

--- script.js
+++ script.js
@@ -3,26 +3,26 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
-const numberOfEnemies = 100;
+const numberOfEnemies = 10;
 const enemiesArray = [];
-const enemyImage = new Image();
-enemyImage.src = './enemies/enemy1.png';
 let gameFrame = 0;
 class Enemy {
   constructor() {
-    this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
-    this.speed = Math.random() * 4 - 2;
+    this.image = new Image();
+    this.image.src = './enemies/enemy1.png';
+    // this.speed = Math.random() * 4 - 2;
     this.spriteWidth = 293;
     this.spriteHeight = 155;
     this.width = this.spriteWidth / 2.5;
     this.height = this.spriteHeight / 2.5;
+    this.x = Math.random() * (canvas.width - this.width);
+    this.y = Math.random() * (canvas.height - this.height);
     this.frame = 0;
     this.flapSpeed = Math.floor(Math.random() * 3 + 1);
   }
   update() {
-    this.x += this.speed;
-    this.y += this.speed;
+    this.x += Math.random() * 15 - 7.5;
+    this.y += Math.random() * 5 - 2.5;
     //animate sprites
     if (gameFrame % this.flapSpeed === 0) {
       this.frame > 4 ? (this.frame = 0) : this.frame++;
@@ -30,7 +30,7 @@ class Enemy {
   }
   draw() {
     ctx.drawImage(
-      enemyImage,
+      this.image,
       this.frame * this.spriteWidth,
       0,
       this.spriteWidth,
